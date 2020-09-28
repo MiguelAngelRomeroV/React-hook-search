fa3585f3305ba25626d67e8c965a185f6b560127
MiguelAngelRomeroV <romerovillonmiguelangel2@gmail.com>
2020-09-28 18:58:40 -0500

    tercer commit

---
+++ docs/precache-manifest.2e6593e3020e7adeb6f836fed0226921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8538a2baf5f15b2a073430283aaeb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a633b6c137f9d93717",
+    "url": "/static/css/main.ad6898d6.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a633b6c137f9d93717",
+    "url": "/static/js/main.0f1be4ff.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
